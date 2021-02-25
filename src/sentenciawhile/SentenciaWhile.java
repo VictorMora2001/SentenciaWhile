7cff08134f1a432b3d53e7b83611202f4e7437dc
amigo <amigo@192.168.1.111>
2021-02-24 20:42:57 -0600

    Hola

---
+++ src/sentenciawhile/SentenciaWhile.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sentenciawhile;
+
+/**
+ *
+ * @author amigo
+ */
+public class SentenciaWhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int a = 0;
+       while (a < 94){
+           if (a%2==0){
+               System.out.println("a = " + a);
+               
+               
+           }
+           
+           a++;
+       }
+    }
+    
+}
